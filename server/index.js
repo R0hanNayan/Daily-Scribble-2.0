6ba713035901ad44adb07e574e944e3a04fbea51
Rohan Nayan <rohannayan405@gmal.com>
2023-09-08 19:29:16 +0530

    Update Server Index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,18 @@ app.use(express.json());
 app.use(cors());
 dotenv.config();
 
+//To serve the frontend
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
+
 //Mongoose Setup:
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
