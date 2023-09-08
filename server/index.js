805bdf2ac1eaf6103e142ebbea691c21d3b49d71
Rohan Nayan <rohannayan405@gmal.com>
2023-09-08 19:39:50 +0530

    Update Server Index.js

--- server/index.js
+++ server/index.js
@@ -16,10 +16,10 @@ app.use(cors());
 dotenv.config();
 
 //To serve the frontend
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
+    path.join(__dirname, "../client/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
