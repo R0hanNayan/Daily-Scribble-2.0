129e7a0087a9b4846b1fe7c8d4b864abc2ee329a
Rohan Nayan <rohannayan405@gmal.com>
2023-09-17 17:13:15 +0530

    Backend Fetch URL Fixes

--- client/src/URL.js
+++ client/src/URL.js
@@ -1,2 +1,3 @@
-const URL = "https://dailyscribble.onrender.com";
+// const URL = "https://dailyscribble.onrender.com";
+const URL = "https://super-seal-pantyhose.cyclic.cloud";
 export default URL;
